c800c6599f3bf69c72e9331dbcbb882e0750be85
NevrotovaLiza <nevrotoval@mail.ru>
2022-06-18 02:52:51 +0700

    8 activity

---
+++ app/src/main/java/com/example/nevrotova_pr12/MainActivity8.kt
@@ -0,0 +1,11 @@
+package com.example.nevrotova_pr12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main8)
+    }
+}
